af81b404269eb76c468b840de2afea2d48891f24
Takmin <jtm0609@naver.com>
2025-05-05 18:15:14 +0900

    [fix] 검색화면에서 탭 바 변환 후 돌아왔을 때 키워드 상태 초기화되는 버그 수정

--- feature/search/src/main/java/com/example/search/SearchScreen.kt
+++ feature/search/src/main/java/com/example/search/SearchScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -34,7 +35,7 @@ fun SearchScreen(
 ) {
     val state by viewModel.uiState.collectAsStateWithLifecycle()
     val searchPagingItems = viewModel.searchResultFlow.collectAsLazyPagingItems()
-    var queryState by remember { mutableStateOf("") }
+    var queryState by rememberSaveable { mutableStateOf("") }
     val effectFlow = viewModel.effect
 
     LaunchedEffect(effectFlow) {
