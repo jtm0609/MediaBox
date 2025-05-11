a7691ba041269b770c38f7e8352dc68f5a69d214
Takmin <jtm0609@naver.com>
2025-05-11 12:51:40 +0900

    [refactor] 검색 결과 처리 로직 수정

--- domain/src/main/java/com/example/domain/usecase/GetSearchResultUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/GetSearchResultUseCase.kt
@@ -3,20 +3,17 @@ package com.example.domain.usecase
 import androidx.paging.PagingData
 import com.example.domain.model.SearchResult
 import com.example.domain.repository.SearchRepository
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flattenMerge
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOf
 import javax.inject.Inject
 
-@OptIn(ExperimentalCoroutinesApi::class)
 class GetSearchResultUseCase @Inject constructor(
     private val searchRepository: SearchRepository,
 ) {
 
-    suspend operator fun invoke(keyword: String): Flow<PagingData<SearchResult>> = flowOf(
-        searchRepository.getInitSearchResults(keyword),
-        searchRepository.getTotalSearchResults(keyword)
-    ).flattenMerge()
+    operator fun invoke(keyword: String): Flow<PagingData<SearchResult>> = flow {
+        emit(searchRepository.getInitSearchResults(keyword).first())
+        emit(searchRepository.getTotalSearchResults(keyword).first())
+    }
 }
